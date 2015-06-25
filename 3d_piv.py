5e2969028dea84cfeff3d66c203bce0e1e202c5d
Callum Kift <callumkift@gmail.com>
2015-06-25 09:46:18 +0200

    Function to find sub-directories added

--- 3d_piv.py
+++ 3d_piv.py
@@ -12,3 +12,19 @@ import numpy as np
 import os
 from math import sqrt
 import matplotlib.pyplot as plt
+
+
+def get_subdirectories(a_dir):
+	"""
+	The input is a direcotry. Retrieves the sub-directories of the folder. Returns a list of
+	subdirectories.
+	"""
+	return [a_dir+name+"/" for name in os.listdir(a_dir)
+			if os.path.isdir(os.path.join(a_dir, name))]
+
+
+
+if __name__ == '__main__':
+
+	main_dir = "/home/callumkift/Documents/sharks_dtu/micro_piv/20150609_x20_bdft_os/"
+	sub_dirs = get_subdirectories(main_dir)

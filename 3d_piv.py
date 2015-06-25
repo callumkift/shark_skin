d02229d2018621a2979c6b49f87f0b4fe1d63cc7
Callum Kift <callumkift@gmail.com>
2015-06-25 10:01:19 +0200

    Function that creates a dictionary with a list of files that are in each subdir.

--- 3d_piv.py
+++ 3d_piv.py
@@ -10,7 +10,8 @@
 
 import numpy as np
 import os
-from math import sqrt
+import collections
+import math
 import matplotlib.pyplot as plt
 
 
@@ -22,9 +23,24 @@ def get_subdirectories(a_dir):
 	return [a_dir+name+"/" for name in os.listdir(a_dir)
 			if os.path.isdir(os.path.join(a_dir, name))]
 
+def get_files(a_dir):
+	gf = []
+	for file in os.listdir(dir):
+		if file.endswith(".txt"):
+			gf.append(dir + "/" + str(file))
+	return gf
 
 
 if __name__ == '__main__':
 
-	main_dir = "/home/callumkift/Documents/sharks_dtu/micro_piv/20150609_x20_bdft_os/"
+	main_dir = "/home/callumkift/Documents/sharks_dtu/micro_piv/20150609_x20_bdft_os/" #main dir where all the subdirs with the data are
 	sub_dirs = get_subdirectories(main_dir)
+	
+	height_file_dict = {}
+	for dir in sub_dirs:
+		height_file_dict["height{0}".format(dir)]=get_files(dir)
+
+	height_file_dict = collections.OrderedDict(sorted(height_file_dict.items()))
+
+	for key in height_file_dict:
+		print key
